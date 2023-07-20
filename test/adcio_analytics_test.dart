8e55eab5a710f26f47098abc4088ab34401aaf8a
adbr <adbr.dev@gmail.com>
2023-07-20 12:22:17 +0900

    test: add test

--- test/adcio_analytics_test.dart
+++ test/adcio_analytics_test.dart
@@ -1,1 +1,38 @@
-void main() {}
+import 'package:adcio_analytics/adcio_analytics.dart';
+import 'package:adcio_analytics/src/errors.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test(
+    'call event int not initialized',
+    () {
+      expect(
+          AdcioAnalytics.clickLogEvent(
+            option: LogOption(
+              requestId: 'requestId',
+              cost: 1000,
+              campaignId: 'campaignId',
+              productId: 'productId',
+            ),
+          ),
+          isA<NotInitializedError>());
+    },
+  );
+
+  test(
+    'call event int initialized',
+    () {
+      AdcioAnalytics.init();
+      expect(
+        AdcioAnalytics.clickLogEvent(
+          option: LogOption(
+              requestId: 'requestId',
+              cost: 1000,
+              campaignId: 'campaignId',
+              productId: 'productId'),
+        ),
+        {'success': 'true'},
+      );
+    },
+  );
+}
