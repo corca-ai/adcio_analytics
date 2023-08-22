6ad5522a3dfb29e68d45e1df565f477a8554c373
adbr <adbr.dev@gmail.com>
2023-08-22 19:01:47 +0900

    feat: add AdcioLogDetector (call to onClick, onImpression)

--- lib/adcio_analytics.dart
+++ lib/adcio_analytics.dart
@@ -1,11 +1,18 @@
 library adcio_analytics;
 
 export 'package:adcio_analytics/src/adcio_log_option.dart';
+export 'package:adcio_analytics/src/adcio_log_detector.dart';
 import 'package:adcio_analytics/adcio_analytics.dart';
 import 'package:adcio_analytics/src/api_client.dart';
 
 class AdcioAnalytics {
   AdcioAnalytics._();
+  static final _impressionHistory = <String>[];
+
+  static bool hasImpression(String adsetId) =>
+      _impressionHistory.contains(adsetId);
+
+  static void clearImpressionHistory() => _impressionHistory.clear();
 
   /// click event log
   static void onClick(
@@ -23,6 +30,8 @@ class AdcioAnalytics {
     AdcioLogOption option, {
     String? baseUrl,
   }) {
+    _impressionHistory.add(option.adsetId);
+
     ImpressionApiClient(baseUrl: baseUrl).call(
       requestId: option.requestId,
       adsetId: option.adsetId,
---
+++ lib/src/adcio_log_detector.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/widgets.dart';
+import 'package:visibility_detector/visibility_detector.dart';
+
+import '../adcio_analytics.dart';
+
+/// Wrap the Widget that displays the recommended product
+/// from [adcio_placement](https://pub.dev/packages/adcio_placement) with an [AdcioLogDetector].
+///
+/// This automatically triggers the onImpression logging event
+/// when the recommended product is displayed on the screen.
+///
+/// Additionally, when the recommended product is clicked
+/// by the customer (similar to a client user), it triggers the onClick logging event.
+class AdcioLogDetector extends StatelessWidget {
+  const AdcioLogDetector({
+    super.key,
+    required this.child,
+    required this.option,
+    this.baseUrl,
+  });
+
+  final Widget child;
+  final AdcioLogOption option;
+  final String? baseUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return VisibilityDetector(
+      key: super.key ?? Key(option.adsetId),
+      onVisibilityChanged: (VisibilityInfo info) {
+        if (info.visibleFraction < 0.5) return;
+        if (AdcioAnalytics.hasImpression(option.adsetId)) return;
+
+        AdcioAnalytics.onImpression(
+          option,
+          baseUrl: baseUrl,
+        );
+      },
+      child: GestureDetector(
+        onTap: () => AdcioAnalytics.onClick(
+          option,
+          baseUrl: baseUrl,
+        ),
+        child: child,
+      ),
+    );
+  }
+}
